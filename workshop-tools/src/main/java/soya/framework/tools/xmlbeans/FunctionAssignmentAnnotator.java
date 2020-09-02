96db52ba71700df178ee31229fe0bcfc168eb29b
soya <wenqunwenqun@gmail.com>
2020-09-02 14:48:04 -0700

    Kafka-Zookeeper Setup

---
+++ workshop-tools/src/main/java/soya/framework/tools/xmlbeans/FunctionAssignmentAnnotator.java
@@ -0,0 +1,58 @@
+package soya.framework.tools.xmlbeans;
+
+import com.google.gson.JsonObject;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import soya.framework.tools.xmlbeans.Buffalo.Annotator;
+import soya.framework.tools.xmlbeans.XmlSchemaBase.MappingNode;
+import soya.framework.tools.xmlbeans.XmlSchemaBase.NodeType;
+
+public class FunctionAssignmentAnnotator implements Annotator<XmlSchemaBase>, MappingFeature {
+    private String function;
+    private List<String> includes;
+    private String field;
+    private String value;
+    private List<String> excludes;
+
+    public FunctionAssignmentAnnotator() {
+    }
+
+    public void annotate(XmlSchemaBase base) {
+        if (this.includes != null) {
+            this.includes.forEach((e) -> {
+                base.get(e);
+            });
+        } else {
+            Set<String> set = new HashSet();
+            if (this.excludes != null) {
+                set.addAll(this.excludes);
+            }
+
+            base.getMappings().entrySet().forEach((e) -> {
+                if (!set.contains(e.getKey())) {
+                    MappingNode node = (MappingNode)e.getValue();
+                    if (!node.getNodeType().equals(NodeType.Folder) && node.getAnnotation("mapping") != null) {
+                        JsonObject mapping = (JsonObject)node.getAnnotation("mapping", JsonObject.class);
+                        String prop = mapping.get(this.field).getAsString();
+                        String assignment = mapping.get("assignment").getAsString();
+                        if (this.value.equalsIgnoreCase(prop)) {
+                            assignment = this.convert(this.function, assignment);
+                            node.annotateAsMappedElement("mapping", "assignment", assignment);
+                        }
+                    }
+                }
+
+            });
+        }
+
+    }
+
+    private String convert(String function, String assignment) {
+        String token;
+        for(token = function; token.contains("$$"); token = token.replace("$$", assignment)) {
+        }
+
+        return token;
+    }
+}
--- workshop-tools/src/main/java/soya/framework/tools/xmlbeans/MappingFeature.java
+++ workshop-tools/src/main/java/soya/framework/tools/xmlbeans/MappingFeature.java
@@ -3,38 +3,43 @@ package soya.framework.tools.xmlbeans;
 public interface MappingFeature {
     String INPUT_ROOT = "$.";
     String FUNCTION_PARAM = "$$";
-
     String GLOBAL_VARIABLE = "global_variable";
     String MAPPING = "mapping";
     String LOOP = "loop";
 
-    class Variable {
+    static class WhileLoop {
+        protected String name;
+        protected String sourcePath;
+        protected String variable;
+        protected MappingFeature.WhileLoop parent;
+        protected int depth = 1;
+
+        public WhileLoop() {
+        }
+
+        protected int getDepth() {
+            return this.parent == null ? this.depth : this.parent.getDepth() + this.depth;
+        }
+    }
+
+    static class Mapping {
+        protected String dataType;
+        protected String cardinality;
+        protected String mappingRule;
+        protected String sourcePath;
+        protected String loop;
+        protected String assignment;
+
+        public Mapping() {
+        }
+    }
+
+    static class Variable {
         protected String name;
         protected String type;
         protected String defaultValue;
 
-    }
-
-    class Mapping {
-        protected String cardinality;
-        protected String mappingRule;
-        protected String sourcePath;
-
-        protected String loop;
-        protected String assignment;
-    }
-
-    class WhileLoop {
-
-        protected String name;
-        protected String sourcePath;
-        protected String variable;
-
-        protected WhileLoop parent;
-        protected int depth = 1;
-        protected int getDepth() {
-            return parent == null ? depth : parent.getDepth() + depth;
+        public Variable() {
         }
     }
-
 }
