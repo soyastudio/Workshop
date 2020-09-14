3874954dbba25e03193ed75bbd8a12a2a40f3a4a
soya <wenqunwenqun@gmail.com>
2020-09-14 07:44:42 -0700

    DeliverySlot update

---
+++ kafka-zookeeper-start.md
@@ -0,0 +1,3 @@
+D:/Kafka-Zookeeper/apache-zookeeper-3.5.6-bin/bin/zkServer
+
+D:/Kafka-Zookeeper/kafka_2.12-2.4.0/bin/windows/kafka-server-start.bat D:/Kafka-Zookeeper/kafka_2.12-2.4.0/config/server.properties
---
+++ workshop-tools/src/main/java/soya/framework/tools/xmlbeans/NodeBuildAnnotator.java
@@ -0,0 +1,13 @@
+package soya.framework.tools.xmlbeans;
+
+public class NodeBuildAnnotator implements Buffalo.Annotator<XmlSchemaBase>, MappingFeature {
+
+    private String path;
+    private String from;
+    private String variable;
+
+    @Override
+    public void annotate(XmlSchemaBase base) {
+
+    }
+}
